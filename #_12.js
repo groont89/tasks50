653870b1ffadc45b56c61d9df0565814c09f422d
Dmitriy <groont@list.ru>
2022-06-16 23:02:09 +0300

    add varian 2 for #_12

--- #_12.js
+++ #_12.js
@@ -3,6 +3,7 @@
 // Например:
 // arrayDiff([1,2],[1]) ==> [2]
 // arrayDiff([1,2,2,2,3],[2]) ==> [1,3]
+//12.1
 function arrayDiff(arr1, arr2) {
     const newArray = arr1.reduce((acc, item) => {
         if (!arr2.includes(item)) {
@@ -11,5 +12,12 @@ function arrayDiff(arr1, arr2) {
         return acc;
     }, []);
     console.log(newArray);
+}
+arrayDiff([1, 2, 2, 2, 3], [2]);
+
+//12.2.
+function arrayDiff(arr1, arr2) {
+   let result = arr1.filter((item) =>!arr2.includes(item));
+   console.log(result);
 }
 arrayDiff([1, 2, 2, 2, 3], [2]);
